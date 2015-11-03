d99b5f8d92a029eefac87f1ddc0dd535b45c1f0b
Ben Upham <bcupham@gmail.com>
2015-11-02 16:00:43 -0800

    Edit and add features following 1st code review

--- js/global.js
+++ js/global.js
@@ -1,1 +1,11 @@
 //global javascript here
+$('#fishModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget);
+  var fish = button.data('fish');
+  var color = button.data('color');
+  var modal = $(this);
+  modal.find('#fish-modal-title').removeClass();
+  modal.find('#fish-modal-title').addClass('modal-title');
+  modal.find('#fish-modal-title').addClass('text-' + color);
+  modal.find('#fish-modal-title').text(fish);
+})
